3ccbd02000628c7d9c693f80731989f3e571f5e2
Ryan <akocryan2@gmail.com>
2024-02-23 01:18:20 +0800

    fixed tutor comment

--- admin/comments.php
+++ admin/comments.php
@@ -1,7 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
 <?php
 include '../components/admin_header.php';
 
-if ($tutor_id = ''){
+if ($tutor_id == ''){
    header('location:login.php');
 }
 
@@ -25,27 +42,7 @@ if(isset($_POST['delete_comment'])){
       $message[] = 'Comment already deleted!';
    }
 }
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard</title>
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
-
-</head>
-<body>
-
-   
 
 <section class="comments">
 
@@ -54,29 +51,31 @@ if(isset($_POST['delete_comment'])){
    
    <div class="show-comments">
       <?php
-         $select_comments = pg_prepare($conn, "select_comments_query", "SELECT * FROM comments WHERE tutor_id = $1");
-         pg_execute($conn, "select_comments_query", array($tutor_id));
-         if(pg_num_rows($select_comments) > 0){
-            while($fetch_comment = pg_fetch_assoc($select_comments)){
-               $select_content = pg_prepare($conn, "select_content_query", "SELECT * FROM content WHERE content_id = $1");
-               pg_execute($conn, "select_content_query", array($fetch_comment['content_id']));
-               $fetch_content = pg_fetch_assoc($select_content);
+      $select_comments = pg_prepare($conn, "select_comments_query", "SELECT * FROM comments WHERE tutor_id = $1");
+      $result_select_comments = pg_execute($conn, "select_comments_query", array($tutor_id));
+
+      if(pg_num_rows($result_select_comments) > 0){
+         $select_content = pg_prepare($conn, "select_content_query", "SELECT * FROM content WHERE content_id = $1");
+         while($fetch_comment = pg_fetch_assoc($result_select_comments)){
+            
+            $result_select_content = pg_execute($conn, "select_content_query", array($fetch_comment['content_id']));
+            $fetch_content = pg_fetch_assoc($result_select_content);
       ?>
       <div class="box" style="<?php if($fetch_comment['tutor_id'] == $tutor_id){echo 'order:-1;';} ?>">
-         <div class="content"><span><?= $fetch_comment['date']; ?></span><p> - <?= $fetch_content['title']; ?> - </p><a href="view_content.php?get_id=<?= $fetch_content['id']; ?>">view content</a></div>
+         <div class="content"><span><?= $fetch_comment['date']; ?></span><p> - <?= $fetch_content['title']; ?> - </p><a href="view_content.php?get_id=<?= $fetch_content['content_id']; ?>">View Content</a></div>
          <p class="text"><?= $fetch_comment['comment']; ?></p>
          <form action="" method="post">
             <input type="hidden" name="comment_id" value="<?= $fetch_comment['comment_id']; ?>">
-            <button type="submit" name="delete_comment" class="inline-delete-btn" onclick="return confirm('delete this comment?');">delete comment</button>
+            <button type="submit" name="delete_comment" class="inline-delete-btn" onclick="return confirm('delete this comment?');">Delete Comment</button>
          </form>
       </div>
       <?php
        }
       }else{
-         echo '<p class="empty">No comments added yet!</p>';
+         echo '<p class="empty">No comments added yet</p>';
       }
       ?>
-      </div>
+   </div>
    
 </section>
 
