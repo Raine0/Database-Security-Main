aa44c2995a98a3da7c243e9c7e1beea4b862b587
Ryan <akocryan2@gmail.com>
2024-03-01 00:05:02 +0800

    Site logo

--- student/watch_video.php
+++ student/watch_video.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Watch Content</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/style.css">
+
+   <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+</head>
+<body>
+
 <?php
 include '../components/user_header.php';
 
@@ -108,24 +126,6 @@ if(isset($_POST['update_now'])){
    }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>watch video</title>
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/style.css">
-
-</head>
-<body>
-
 
 <?php
 if(isset($_POST['edit_comment'])){
